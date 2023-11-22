c530f82634f8a2cb9f95187e407bf98df1baa4b2
kaiquef30 <kaiquefernandosantana@gmail.com>
2023-11-22 11:24:30 -0300

    str-producer started

---
+++ apache-kafka-producer/src/test/java/com/strproducer/apachekafka/ApacheKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strproducer.apachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
